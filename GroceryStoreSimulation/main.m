087683e5c7a5472957e97b85a6a0473489296082
Dan Brajkovic <dbrajkovic@mac.com>
2014-02-21 12:37:39 -0600

    Initial Commit

---
+++ GroceryStoreSimulation/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GroceryStoreSimulation
+//
+//  Created by Dan Brajkovic on 2/21/14.
+//  Copyright (c) 2014 Dan Brajkovic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
